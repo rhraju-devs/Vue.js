17b5950d72cb5d79a5575b3f365b144eedbaf774
Rajib-hossain-cse-rmstu <rajibraju771@gmail.com>
2023-07-12 01:07:48 +0600

    Display Data from object and array

--- module03/demo-project/src/App.vue
+++ module03/demo-project/src/App.vue
@@ -7,11 +7,21 @@
       const date = new Date();
       return date.toDateString()
     }
+
+    const data = {
+      message: 'Welcome. Get ready to master Vue.js 3!',
+      date: '12 Jully, 2023',
+      task: [1,2,3,4],
+    }
 </script>
 
 <template>
 <h1 class="text-danger">{{ message }}</h1>
 <h2>{{ preDateMessage }} <span class="text-bold">{{ getDate() }}</span></h2>
+<h4>Printing object data</h4>
+<h2>{{ data.message  }}</h2>
+<h2>{{ data.date }}</h2>
+<h3>Total number of Task is {{ data.task.length }}</h3>
 </template>
 
 <style scoped>
