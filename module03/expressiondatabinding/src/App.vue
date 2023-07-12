cdca6b4633cfa54295a0b1d52ba114f0ad287dfe
Rajib-hossain-cse-rmstu <rajibraju771@gmail.com>
2023-07-13 00:00:25 +0600

    Javascript Expressions in data binding

---
+++ module03/expressiondatabinding/src/App.vue
@@ -0,0 +1,25 @@
+<script setup>
+  const number = 16
+  const animal = "Tiger"
+  const searchUrl1 = "https://www.pexels.com/search/tiger"
+  const searchUrl2 = "https://www.pexels.com/search/"
+
+</script>
+
+<template>
+  <h2>Javascript Expressions in data binding</h2>
+  <h3>{{ number }} / 4 = {{ number/4 }}</h3>
+  <h3>Number check: Odd or Even</h3>
+  <h3>{{ number }} is {{ number %2 == 0 ? 'Even' : 'Odd' }}</h3>
+<h3>
+  <a :href="searchUrl1" target="_blank" rel="noopener noreferrer">Search for {{  animal }}</a>
+</h3>
+
+<h3>
+  <a target="_blank" :href="`https://www.pexels.com/search/${animal.toLowerCase()}`"> Search for {{ animal }}</a>
+</h3>
+</template>
+
+<style scoped>
+
+</style>
